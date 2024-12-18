af9c508155f9c0e39cc8e699574dce2bd627c12e
BenjaminFreyberg <benjamin.freyberg@gmail.com>
2024-12-18 14:51:51 +0100

    Upload 2 ohne Function

---
+++ script.js
@@ -0,0 +1,330 @@
+let questions = [
+         {
+            "question": "Was bedeutet HTML?",
+            "options": [
+              "Hyper Text Markup Language",
+              "Home Tool Markup Language",
+              "Hyperlinks and Text Markup Language",
+              "Hyper Tool Markup Language"
+            ],
+            "answer": "Hyper Text Markup Language"
+          },
+          {
+            "question": "Welches HTML-Element wird verwendet, um eine Überschrift der Ebene 1 zu definieren?",
+            "options": [
+              "<head>",
+              "<h1>",
+              "<header>",
+              "<title>"
+            ],
+            "answer": "<h1>"
+          },
+          {
+            "question": "Was ist die korrekte Syntax für einen Kommentar in HTML?",
+            "options": [
+              "<!-- Kommentar -->",
+              "// Kommentar",
+              "/* Kommentar */",
+              "# Kommentar"
+            ],
+            "answer": "<!-- Kommentar -->"
+          },
+          {
+            "question": "Welches Attribut wird verwendet, um einen Link in einem neuen Tab zu öffnen?",
+            "options": [
+              "target=\"_blank\"",
+              "target=\"new\"",
+              "target=\"tab\"",
+              "new-tab=\"true\""
+            ],
+            "answer": "target=\"_blank\""
+          },
+          {
+            "question": "Welche Sprache wird verwendet, um interaktive Elemente zu einer Website hinzuzufügen?",
+            "options": [
+              "CSS",
+              "HTML",
+              "JavaScript",
+              "XML"
+            ],
+            "answer": "JavaScript"
+          },
+          {
+            "question": "Wie definiert man eine Variable in JavaScript?",
+            "options": [
+              "let variableName;",
+              "variable variableName;",
+              "var: variableName;",
+              "define variableName;"
+            ],
+            "answer": "let variableName;"
+          },
+          {
+            "question": "Was ist das Ergebnis von 2 + '3' in JavaScript?",
+            "options": [
+              "5",
+              "23",
+              "Fehler",
+              "undefined"
+            ],
+            "answer": "23"
+          },
+          {
+            "question": "Welches Attribut wird verwendet, um einem HTML-Element eine CSS-Klasse zuzuweisen?",
+            "options": [
+              "id",
+              "style",
+              "class",
+              "type"
+            ],
+            "answer": "class"
+          },
+          {
+            "question": "Wie fügt man in HTML ein Bild hinzu?",
+            "options": [
+              "<img src=\"url\">",
+              "<image url=\"url\">",
+              "<img link=\"url\">",
+              "<picture src=\"url\">"
+            ],
+            "answer": "<img src=\"url\">"
+          },
+          {
+            "question": "Welches Zeichenpaar wird in JavaScript für Template Literals verwendet?",
+            "options": [
+              "\"\"",
+              "''",
+              "``",
+              "<>"
+            ],
+            "answer": "``"
+          },
+          {
+            "question": "Welches Ereignis wird ausgelöst, wenn ein Benutzer auf ein HTML-Element klickt?",
+            "options": [
+              "onhover",
+              "onclick",
+              "onpress",
+              "onchange"
+            ],
+            "answer": "onclick"
+          },
+          {
+            "question": "Wie bindet man eine externe JavaScript-Datei in HTML ein?",
+            "options": [
+              "<script src=\"datei.js\"></script>",
+              "<javascript file=\"datei.js\"></javascript>",
+              "<js href=\"datei.js\"></js>",
+              "<link js=\"datei.js\"></link>"
+            ],
+            "answer": "<script src=\"datei.js\"></script>"
+          },
+          {
+            "question": "Wie lautet die Kurzform für eine anonyme Funktion in JavaScript?",
+            "options": [
+              "Lambda-Funktion",
+              "Arrow-Funktion",
+              "Callback-Funktion",
+              "Higher-Order-Funktion"
+            ],
+            "answer": "Arrow-Funktion"
+          },
+          {
+            "question": "Welches HTML-Element wird für den Hauptinhalt einer Webseite verwendet?",
+            "options": [
+              "<section>",
+              "<main>",
+              "<article>",
+              "<div>"
+            ],
+            "answer": "<main>"
+          },
+          {
+            "question": "Wie nennt man den Standardobjekttyp in JavaScript?",
+            "options": [
+              "JSON",
+              "Array",
+              "Object",
+              "Function"
+            ],
+            "answer": "Object"
+          },
+          {
+            "question": "Welche Methode wird verwendet, um ein Element nach seiner ID zu finden?",
+            "options": [
+              "document.querySelector()",
+              "document.getElementById()",
+              "document.getElementsByClass()",
+              "document.getById()"
+            ],
+            "answer": "document.getElementById()"
+          },
+          {
+            "question": "Wie erstellt man eine Schleife in JavaScript?",
+            "options": [
+              "for", 
+              "while", 
+              "do-while", 
+              "Alle der genannten Optionen"
+            ],
+            "answer": "Alle der genannten Optionen"
+          },
+          {
+            "question": "Wie bindet man Inline-CSS in ein HTML-Element ein?",
+            "options": [
+              "<div class=\"...\">",
+              "<div style=\"...\">",
+              "<div css=\"...\">",
+              "<div id=\"...\">"
+            ],
+            "answer": "<div style=\"...\">"
+          },
+          {
+            "question": "Welches Ereignis tritt ein, wenn eine Seite vollständig geladen wurde?",
+            "options": [
+              "onload",
+              "onstart",
+              "onready",
+              "onexecute"
+            ],
+            "answer": "onload"
+          },
+          {
+            "question": "Was ist der Standardwert für das 'position'-Attribut in CSS?",
+            "options": [
+              "relative",
+              "absolute",
+              "fixed",
+              "static"
+            ],
+            "answer": "static"
+          },
+          {
+            "question": "Welche Methode wird verwendet, um einen neuen Listeneintrag (li) zu erstellen?",
+            "options": [
+              "document.createElement('li')",
+              "document.newItem('li')",
+              "document.create('li')",
+              "document.addElement('li')"
+            ],
+            "answer": "document.createElement('li')"
+          },
+          {
+            "question": "Wie beendet man eine Funktion in JavaScript?",
+            "options": [
+              "stop;",
+              "end;",
+              "return;",
+              "break;"
+            ],
+            "answer": "return;"
+          },
+          {
+            "question": "Welcher HTML-Tag wird verwendet, um einen Dropdown auszuwählen?",
+            "options": [
+              "<input>",
+              "<select>",
+              "<dropdown>",
+              "<option>"
+            ],
+            "answer": "<select>"
+          },
+          {
+            "question": "Wie kann man den Inhalt eines HTML-Elements ändern?",
+            "options": [
+              "element.innerHTML",
+              "element.value",
+              "element.content",
+              "element.text"
+            ],
+            "answer": "element.innerHTML"
+          },
+          {
+            "question": "Wie überprüft man, ob eine Variable einen Wert hat?",
+            "options": [
+              "if(variable)",
+              "if(variable != null)",
+              "if(variable !== undefined)",
+              "Alle der genannten Optionen"
+            ],
+            "answer": "Alle der genannten Optionen"
+          },
+          {
+            "question": "Wie fügt man ein Skript ein, das ausgeführt wird, bevor die Seite vollständig geladen ist?",
+            "options": [
+              "<script defer>",
+              "<script async>",
+              "<script load>\n",
+              "<script immediate>"
+            ],
+            "answer": "<script async>"
+          },
+          {
+            "question": "Wie entfernt man ein Element aus dem DOM?",
+            "options": [
+              "element.remove()",
+              "element.delete()",
+              "element.destroy()",
+              "element.clear()"
+            ],
+            "answer": "element.remove()"
+          },
+          {
+            "question": "Welche der folgenden Funktionen konvertiert eine Zeichenkette in eine Zahl?",
+            "options": [
+              "parseInt()",
+              "toNumber()",
+              "convertToNumber()",
+              "NumberParse()"
+            ],
+            "answer": "parseInt()"
+          },
+          {
+            "question": "Welche Methode wird verwendet, um alle Elemente mit einer bestimmten Klasse zu finden?",
+            "options": [
+              "document.getElementsByClassName()",
+              "document.querySelector()",
+              "document.getElementsByName()",
+              "document.getClass()"
+            ],
+            "answer": "document.getElementsByClassName()"
+          }
+        ];
+
+let currentQuestion = 0;
+function init(){
+    
+    document.getElementById('allQuestions').innerHTML = questions.length;
+
+    showCurrentQuestion();
+}
+
+function showCurrentQuestion(){
+
+    let question = questions[currentQuestion];
+ 
+    document.getElementById ('questionText').innerHTML = question['question'];
+    document.getElementById ('answerOption1').innerHTML = questions[0]['options'][0];
+    document.getElementById ('answerOption2').innerHTML = questions[0]['options'][1];
+    document.getElementById ('answerOption3').innerHTML = questions[0]['options'][2];
+    document.getElementById ('answerOption4').innerHTML = questions[0]['options'][3];
+}
+
+function answerQuestion(selection){
+
+    let question = questions[currentQuestion];
+    let selectedAnswer = selection;
+
+    console.log('selectedAnswer is', selectedAnswer);
+    console.log('Correct Answer is' , questions[0]['answer']);
+    
+    if ( selectedAnswer == questions[0]['options'][0]){
+        console.log('Correct');
+    } else {
+        console.log('false');
+        
+    }
+    ;
+      
+
+}
